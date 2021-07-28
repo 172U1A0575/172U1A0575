17eee0676640609c34b9db849397dc52808c9218
Gangireddy Sowmya <you@example.com>
2021-07-28 17:12:35 +0530

    code challenge

---
+++ code challenge/src/challenge1/four.java
@@ -0,0 +1,16 @@
+package challenge1;
+import java.lang.Math;
+
+public class four {
+	static double hypo(double a,double b)
+	{
+		double c=(float)Math.pow((a*a+b*b),0.5);
+		return c;
+	}
+	public static void main(String[] arrgs) {
+		System.out.println((int)hypo(3,4));
+		System.out.println(hypo(34.5,23.6));
+	}
+}
+
+	
