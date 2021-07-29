11aa24e522800aa92ace3b197c2ff2441c274c89
Gangireddy Sowmya <you@example.com>
2021-07-29 21:55:27 +0530

    task1

---
+++ practice/src/eg1/Datatypes.java
@@ -0,0 +1,30 @@
+package eg1;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		int x=100;
+		System.out.println(" x= " + x);
+		float y=2.00f;
+		System.out.println(" value of y= " + y);
+		long contact=177889L;
+		System.out.println(" contact= " + contact);
+		int a=100;
+		System.out.println("a = " +a);
+		float b=a;
+		System.out.println("b = "+b);
+		long c=a;
+		System.out.println("c =" +c);
+		boolean e=false;
+		System.out.println(e);
+		float f=3.45f;
+		System.out.println("f "+ f);
+		long r=c;
+		System.out.println("r "+r);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+	}
+}
+
+	
